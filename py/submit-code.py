356c1c465d39612167037e8d333df8a927881bf8
height921 <gzk_personal@163.com>
2020-07-14 11:43:00 +0800

    Initial commit

---
+++ py/submit-code.py
@@ -0,0 +1,7 @@
+#!python
+# -*- encoding:utf-8 -*-
+# Created by admin at 2020/7/14
+
+import re
+import requests
+import threadpool
---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!E:\ACM题集与分析\mysite\mysite\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
