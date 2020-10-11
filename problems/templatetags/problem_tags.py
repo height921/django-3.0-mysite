bb36c1c685046b5703efbdef3bdd17503b38427e
height921 <gzk_personal@163.com>
2020-10-11 17:51:44 +0800

    initial

--- problems/templatetags/problem_tags.py
+++ problems/templatetags/problem_tags.py
@@ -3,5 +3,20 @@
 # Created by admin at 2020/10/11
 
 from django import template
-
+from django.contrib.contenttypes.models import ContentType
+from status.models import Status
 register = template.Library()
+
+
+@register.simple_tag(takes_context=True)
+def get_status(context, obj):
+    user = context['user']
+    content_type = ContentType.objects.get_for_model(obj)
+    if not user.is_authenticated:
+        return ''
+    results = Status.objects.filter(problem=obj, user=user)
+    if results:
+        if results.filter(result="Accepted"):
+            return 'fa-check fa-check-color'
+        return 'fa-question fa-question-color'
+    return ''
